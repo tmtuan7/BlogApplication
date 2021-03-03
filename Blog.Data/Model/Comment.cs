0e7d3ad6ae8b66e15a8a6a082ba37cd79d472ff7
tmtuan7 <tybuonban@gmail.com>
2021-03-03 22:58:50 +0700

    Add project files.

---
+++ Blog.Data/Model/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Data.Model
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
